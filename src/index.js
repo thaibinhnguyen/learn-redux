5e6e08f4243d908b31d578898f9fffa2852dfc79
THAI Binh Nguyen <thaibinhnguyen26041996@gmail.com>
2020-05-22 18:23:06 +0200

    curry technique

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-console.log("Hello World!");
+import { compose, pipe } from 'lodash/fp';
+
+let input = "  JavaScript  ";
+let output = "<div>" + input.trim() + "</div>";
+
+const trim = str => str.trim();
+const wrap = type => str => `<${type}>${str}</${type}>`;
+const toLowerCase = str => str.toLowerCase();
+
+const transform = pipe(trim, toLowerCase, wrap('div'));
+console.log(transform(input));
+// const result = wrapInDiv(toLowerCase(trim(input)));
